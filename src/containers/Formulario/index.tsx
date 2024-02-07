eced19639941c5ce3d175953f6c2375837769600
Pedro Maia <pedrodelmonico@hotmail.com>
2024-02-07 18:38:00 -0300

    finalizando formulário

--- src/containers/Formulario/index.tsx
+++ src/containers/Formulario/index.tsx
@@ -1,24 +1,78 @@
+import { useState, FormEvent } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 import * as S from './styles'
 import { Titulo } from '../../styles'
+import { adicionar } from '../../store/reducers/produtos'
 
 const Formulario = () => {
+  const [nome, setNome] = useState('')
+  const [codigo, setCodigo] = useState(0)
+  const [descricao, setDescricao] = useState('')
+  const [preco, setPreco] = useState(0)
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const cadastra = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (nome.length > 0 && codigo > 0 && descricao.length > 0 && preco > 0) {
+      dispatch(
+        adicionar({
+          nome,
+          codigo,
+          descricao,
+          preco
+        })
+      )
+      alert('produto adicionado com sucesso')
+      navigate('/')
+    } else {
+      alert('dados inválidos')
+    }
+  }
   return (
     <S.Container>
       <Titulo>CADASTRO DE PRODUTO</Titulo>
-      <S.Formulario>
+      <S.Formulario onSubmit={cadastra}>
         <S.Campo>
           <label htmlFor="nome">Nome do produto</label>
-          <input type="text" id="nome" />
+          <input
+            type="text"
+            id="nome"
+            value={nome}
+            onChange={({ target }) => setNome(target.value)}
+          />
         </S.Campo>
         <S.Campo>
           <label htmlFor="codigo">Código do produto</label>
-          <input type="text" id="codigo" />
+          <input
+            type="text"
+            id="codigo"
+            value={isNaN(codigo) || codigo === 0 ? '' : codigo}
+            onChange={({ target }) => setCodigo(Number(target.value))}
+          />
         </S.Campo>
-        <textarea placeholder="digite a descrição do produto" />
+        <S.Campo>
+          <S.Descricao
+            placeholder="digite a descrição do produto"
+            value={descricao}
+            onChange={({ target }) => setDescricao(target.value)}
+          />
+        </S.Campo>
         <S.Campo>
           <label htmlFor="preco">Preço do produto</label>
-          <input type="text" id="preco" />
+          <input
+            placeholder="ex: 149.9"
+            type="text"
+            id="preco"
+            value={isNaN(preco) || preco === 0 ? '' : preco}
+            onChange={({ target }) => setPreco(Number(target.value))}
+          />
         </S.Campo>
+        <S.BotaoEnviar type="submit">Cadastrar</S.BotaoEnviar>
       </S.Formulario>
     </S.Container>
   )
--- src/containers/Formulario/styles.ts
+++ src/containers/Formulario/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import variaveis from '../../styles/variaveis'
+import { Botao } from '../../styles'
 
 export const Container = styled.div`
   padding: 25px;
@@ -11,12 +12,48 @@ export const Formulario = styled.form`
   width: 100%;
   height: 90vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-itens: center;
+  align-items: center;
 `
 
 export const Campo = styled.div`
+  width: 40vw;
+  margin-bottom: 25px;
+
   label {
     display: block;
+    margin-bottom: 10px;
   }
+
+  input {
+    width: 100%;
+    height: 4vh;
+    padding: 10px;
+    border-radius: 15px;
+    border: none;
+    background-color: ${variaveis.branco};
+
+    &:focus {
+      outline: 1px solid #000;
+      background-color: #fff;
+    }
+  }
+`
+
+export const Descricao = styled.textarea`
+  width: 100%;
+  height: 10vh;
+  border-radius: 15px;
+  padding: 15px;
+  background-color: ${variaveis.branco};
+
+  &:focus {
+    outline: 1px solid #000;
+    background-color: #fff;
+  }
+`
+
+export const BotaoEnviar = styled(Botao)`
+  margin-top: 20px;
 `
